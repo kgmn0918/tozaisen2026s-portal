7c8592f855922e991b45b1e9ca7faae1ee194a18
kgmn0918 <cogito.ergo.sum0918@gmail.com>
2026-08-14 17:53:56 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,9 @@
 
 // ★★★ ここを、GASデプロイ後に発行される Web App の URL に書き換えてください ★★★
 // 例: https://script.google.com/macros/s/AKfycb.../exec
-const GAS_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
-
+// const GAS_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyNBLvFQTlxoDgLJnY7jjLAqq2rHW2aZNDF6SO3DsGXa0YZNCsgtxRQ_EZDnTpvMQZ-pA/exec";
+  
 function sleep(ms) { return new Promise((r) => setTimeout(r, ms)); }
 
 // GET リクエスト(一時的な失敗に備えて最大3回リトライ)
